90185c4a64eb8c15f1d5e2d8a05ac0bd81665716
Anthony Li <anthli1895@gmail.com>
2016-06-07 20:38:57 -0400

    Added a bit more documentation on what's going on

--- GoogleURLShortenerApp/MainWindow.xaml.cs
+++ GoogleURLShortenerApp/MainWindow.xaml.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Windows;
+﻿using System.Windows;
 
 namespace GoogleURLShortenerDesktopGUI {
   public partial class MainWindow : Window {
--- GoogleURLShortenerApp/URLShortener.cs
+++ GoogleURLShortenerApp/URLShortener.cs
@@ -9,15 +9,19 @@ namespace GoogleURLShortenerDesktopGUI {
       // A request needs to be sent to the Google URL Shortener API containing
       // longURL so that the shortened version of it can be returned
       public static string ShortenURL(string longURL) {
+        // Parse the Google API Key from apiKey.json
         var apiKey = JSON.GetAPIKey("apiKey.json");
 
+        // UrlshortenerService with the parse API Key
         service = new UrlshortenerService(new BaseClientService.Initializer() {
           ApiKey = apiKey
         });
 
+        // Request containing the longURL
         var req = new Google.Apis.Urlshortener.v1.Data.Url();
         req.LongUrl = longURL;
 
+        // Insert the request to the API and return the short URL
         return service.Url.Insert(req).Execute().Id;
       }
     }
